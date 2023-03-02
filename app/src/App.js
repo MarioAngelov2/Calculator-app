bbb40a22fd4a415275749575ebeb6060bf9330a7
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-03-02 10:39:56 +0200

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -4,9 +4,7 @@ import Calculator from "./Components/Calculator";
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>Calculator App</h1>
-      </header>
+     
       <main>
         <Calculator />
       </main>
